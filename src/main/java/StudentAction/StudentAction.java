774e659b841be787dcfaba690c6a43883f66ed48
MAX <MAX@LAPTOP-DLI628EI.lan>
2017-12-26 11:22:14 +0800

    Spring annotation OK2

---
+++ src/main/java/StudentAction/StudentAction.java
@@ -0,0 +1,11 @@
+package StudentAction;
+
+import org.springframework.stereotype.Component;
+
+
+@Component("studenActionId")
+public class StudentAction {
+	public void Execute() {
+		System.out.println("..............");
+	}
+}
---
+++ src/main/java/spring10/StudentAction200.java
@@ -0,0 +1,11 @@
+package spring10;
+
+import org.springframework.stereotype.Component;
+
+
+@Component("studenActionId200")
+public class StudentAction200 {
+	public void Execute() {
+		System.out.println("200..............");
+	}
+}
